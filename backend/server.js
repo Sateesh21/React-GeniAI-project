5fb14daae036cec303ce45a051c05eb605c21e5c
sateesh16 <sateeshs1916@gmail.com>
2026-06-30 06:26:01 +0530

    Package file added

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ app.use((req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
